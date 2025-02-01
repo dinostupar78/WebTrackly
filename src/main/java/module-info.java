690c29998b3c16a96ee6fcf3f2c244310aaa1cea
Dino Stupar <dinostupar78@gmail.com>
2025-02-01 01:16:24 +0100

    changes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,6 @@ module hr.javafx.webtrackly {
     requires org.kordamp.bootstrapfx.core;
 
     opens hr.javafx.webtrackly to javafx.fxml;
-    exports hr.javafx.webtrackly;
     exports hr.javafx.webtrackly.main;
     opens hr.javafx.webtrackly.main to javafx.fxml;
 }
