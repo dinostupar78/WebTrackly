ec83ab37bac2a16810c5a1a7085ced9789e678c6
Dino Stupar <dinostupar78@gmail.com>
2025-07-07 09:48:43 +0200

    changes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,13 +2,17 @@ module hr.javafx.webtrackly {
     requires javafx.controls;
     requires javafx.fxml;
 
-    exports hr.javafx.webtrackly.main;
-    exports hr.javafx.webtrackly.controller;
     exports hr.javafx.webtrackly.app.model;
     exports hr.javafx.webtrackly.app.enums;
     exports hr.javafx.webtrackly.app.db;
     exports hr.javafx.webtrackly.app.exception;
+    exports hr.javafx.webtrackly.app.generics;
+    exports hr.javafx.webtrackly.app.files;
+    exports hr.javafx.webtrackly.main;
+    exports hr.javafx.webtrackly.controller;
+    exports hr.javafx.webtrackly.threads;
     exports hr.javafx.webtrackly.utils;
+
 
     requires org.kordamp.bootstrapfx.core;
     requires org.slf4j;
@@ -18,7 +22,6 @@ module hr.javafx.webtrackly {
 
     opens hr.javafx.webtrackly.controller to javafx.fxml;
     opens hr.javafx.webtrackly to javafx.fxml;
-    exports hr.javafx.webtrackly.main;
     opens hr.javafx.webtrackly.main to javafx.fxml;
     opens hr.javafx.webtrackly.app.model to javafx.base;
 }
